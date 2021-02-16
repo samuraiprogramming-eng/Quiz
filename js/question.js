2727219fd134b86ac5ca1a8912fb1a9ee8a37a6d
samuraiprogramming-eng <sohaibfathy33@gmail.com>
2021-02-16 10:15:07 +0200

    quiz

---
+++ js/question.js
@@ -0,0 +1,61 @@
+
+
+ // Array of Objects
+ const quiz = [
+  {
+  	q:'Which month comes right before june ?',
+  	options:['may','september','july','august'],
+  	answer:0
+  },
+  {
+  	q:'What color is a banana?',
+  	options:['red','yellow','white','blue'],
+  	answer:1
+  },
+  {
+  	q:'3 + 4 = 7 ?',
+  	options:['true','false'],
+  	answer:0
+  },
+  {
+  	q:'What time of day do we have breakfast?',
+  	options:['In the afternoon','In the evening','In the morning'],
+  	answer:2
+  },
+  {
+  	q:'What is 22 + 6?',
+  	options:['99','56','16','28'],
+  	answer:3
+  },
+  {
+    q:'How many squares are there in the following figure?',
+    options:['35','30','40','50'],
+    answer:2,
+    img:'img/square.jpg'
+  },
+  {
+    q:'Count the triangles in picture below.',
+    options:['7','9','12','13'],
+    answer:3,
+    img:'img/triangle.jpg'
+  },
+  {
+    q:'Windows, macOS, and Linux are examples of ________.?',
+    options:['web browsers','mobile devices','filmy heroines','operating systems'],
+    answer:3,
+  },
+  {
+    q:'What does "GUI" stand for?',
+    options:['Global user index','Graphical user interface','golu use iphone'],
+    answer:1,
+  },
+  {
+    q:'Mark Zuckerberg is the owner of  ?',
+    options:['facebook','google','linux','linkedin'],
+    answer:0,
+  }
+ ]
+
+
+
+
